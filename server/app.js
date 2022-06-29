2720d2f5a5a495b9279e38dd08b4b1de85e20ae8
Andrés Siri <andres.siri@hotmail.com>
2022-06-29 15:07:23 -0300

    Setup PostgreSQL database

--- server/app.js
+++ server/app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const session = require('express-session');
-require('dotenv').config();
+require('custom-env').env(process.env.NODE_ENV);
+const devEnvironment = process.env.NODE_ENV === 'development';
 
 const app = express();
+
+// Test database connection
+const db = require('./models');
+db.sequelize.authenticate()
+  .then(() => {
+    if (devEnvironment) console.log('Database is connected...'); // eslint-disable-line no-console
+  })
+  .catch(err => {
+    if (devEnvironment) { console.log('DB Error: ' + err); }; // eslint-disable-line no-console
+  });
 
 // body parser middleware
 app.use(express.json());
---
+++ server/config/configDB.js
@@ -0,0 +1,26 @@
+require('custom-env').env(process.env.NODE_ENV);
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres'
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  }
+};

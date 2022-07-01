4a3a77a090c1eb4b1b847e077ed6d9c6d89e064a
Andrés Siri <andres.siri@hotmail.com>
2022-07-01 16:57:05 -0300

    Setup eslint for react

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,32 @@
 module.exports = {
   env: {
     es2021: true,
-    node: true
+    node: true,
+    browser: true
+  },
+  settings: {
+    react: {
+      version: '18.2.0'
+    }
   },
   extends: [
-    'standard'
+    'standard',
+    'plugin:react/recommended'
+  ],
+  plugins: [
+    'react'
   ],
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
   rules: {
     semi: [2, 'always'],
     'no-console': 'error',
-    quotes: [2, 'single', { avoidEscape: true }]
+    quotes: [2, 'single', { avoidEscape: true }],
+    'react/jsx-indent': ['error', 2]
   }
 };
